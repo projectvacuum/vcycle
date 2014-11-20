71d38f89b21c4b4c44faf83a98a14b631c9c3507
Andrew McNab <andrew.mcnab@cern.ch>
2014-11-20 21:37:02 +0000

    Add log_machineoutputs option to vmtypes

--- VCYCLE.py
+++ VCYCLE.py
@@ -151,6 +151,12 @@ def readConf(requirePassword=True):
               except:
                 return 'Option ' + opt + ' required in [' + vmtypeSectionName + ']'
 
+            for opt in vmtypeBooleanOptions:
+              try:
+                vmtype[opt] = int(parser.get(vmtypeSectionName, opt))
+              except:
+                return 'Option ' + opt + ' required in [' + vmtypeSectionName + ']'
+
             try:
               vmtype['heartbeat_file'] = parser.get(vmtypeSectionName, 'heartbeat_file')
             except:
@@ -166,6 +172,12 @@ def readConf(requirePassword=True):
             except:
               return 'Option target_share required in [' + vmtypeSectionName + ']'
             
+            if parser.has_option(vmtypeSectionName, 'log_machineoutputs') and \
+               parser.get(vmtypeSectionName, 'log_machineoutputs').strip().lower() == 'true':
+              vmtype['log_machineoutputs'] = True
+            else:
+              vmtype['log_machineoutputs'] = False
+
             if spaceName not in lastFizzles:
               lastFizzles[spaceName] = {}
               
@@ -203,3 +215,40 @@ def createFile(targetname, contents, mode=None):
 def logLine(text):
   sys.stderr.write(time.strftime('%b %d %H:%M:%S [') + str(os.getpid()) + ']: ' + text + '\n')
   sys.stderr.flush()
+
+def logMachineoutputs(hostName, vmtypeName, spaceName):
+
+  if os.path.exists('/var/lib/vcycle/machineoutputs/' + spaceName + '/' + vmtypeName + '/' + hostName):
+    # Copy (presumably) already exists so don't need to do anything
+    return
+   
+  try:
+    # Get the list of files that the VM wrote in its /etc/machineoutputs
+    outputs = os.listdir('/var/lib/vcycle/machines/' + hostName + '/machineoutputs')
+  except:
+    logLine('Failed reading /var/lib/vcycle/machines/' + hostName + '/machineoutputs')
+    return
+        
+  try:
+    os.makedirs('/var/lib/vcycle/machineoutputs/' + spaceName + '/' + vmtypeName + '/' + hostName,
+                stat.S_IWUSR + stat.S_IXUSR + stat.S_IRUSR + stat.S_IXGRP + stat.S_IRGRP + stat.S_IXOTH + stat.S_IROTH)
+  except:
+    logLine('Failed creating /var/lib/vcycle/machineoutputs/' + spaceName + '/' + vmtypeName + '/' + hostName)
+    return
+      
+  if outputs:
+    # Go through the files one by one, adding them to the machineoutputs directory
+    for oneOutput in outputs:
+
+      try:
+        # first we try a hard link, which is efficient in time and space used
+        os.link('/var/lib/vcycle/machines/' + hostName + '/machineoutputs/' + oneOutput,
+                '/var/lib/vcycle/machineoutputs/' + spaceName + '/' + vmtypeName + '/' + hostName + '/' + oneOutput)
+      except:
+        try:
+          # if linking failed (different filesystems?) then we try a copy
+          shutil.copyfile('/var/lib/vcycle/machines/' + hostName + '/machineoutputs/' + oneOutput,
+                          '/var/lib/vcycle/machineoutputs/' + spaceName + '/' + vmtypeName + '/' + hostName + '/' + oneOutput)
+        except:
+          logLine('Failed copying /var/lib/vcycle/machines/' + hostName + '/machineoutputs/' + oneOutput + 
+                  ' to /var/lib/vcycle/machineoutputs/' + spaceName + '/' + vmtypeName + '/' + hostName + '/' + oneOutput)
